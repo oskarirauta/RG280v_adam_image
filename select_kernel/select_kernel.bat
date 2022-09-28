8bd407b667c60852b2093dce669d01567d832a07
Oskari Rauta <oskari.rauta@gmail.com>
2022-09-28 11:56:31 +0300

    Update select_kernel.bat

--- select_kernel/select_kernel.bat
+++ select_kernel/select_kernel.bat
@@ -2,7 +2,8 @@
 
 :again
    echo     1: RG280V
-   echo     2: RG280M
+   echo     2: RG280M-v1.0
+   echo     2: RG280M-v1.1
    echo     3: RG350/P
    echo     4: RG350M
    echo     5: PocketGo2 v2
@@ -13,26 +14,30 @@
 	goto install_kernel
    )
    if /i "%answer:~,1%" EQU "2" (
-    set dir=rg280m
+    set dir=rg280m-v1.0
 	goto install_kernel
    )
    if /i "%answer:~,1%" EQU "3" (
+    set dir=rg280m-v1.1
+	goto install_kernel
+   )   
+   if /i "%answer:~,1%" EQU "4" (
     set dir=rg350
 	goto install_kernel
    )
-   if /i "%answer:~,1%" EQU "4" (
+   if /i "%answer:~,1%" EQU "5" (
     set dir=rg350m
 	goto install_kernel
    )
-   if /i "%answer:~,1%" EQU "5" (
+   if /i "%answer:~,1%" EQU "6" (
     set dir=pocketgo2v2
 	goto install_kernel
    )
-   if /i "%answer:~,1%" EQU "6" (
+   if /i "%answer:~,1%" EQU "7" (
     set dir=rg300x
 	goto install_kernel
    )
-   echo Please type 1, 2, 3, 4, 5 or 6
+   echo Please type 1, 2, 3, 4, 5, 6 or 7
    goto again
 
 :install_kernel
